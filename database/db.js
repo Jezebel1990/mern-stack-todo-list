7a667fe436e9a9409dc23133de5359704e5b80aa
Jezebel1990 <jezebelguedes@gmail.com>
2023-06-16 00:51:06 -0300

    connecte database

---
+++ database/db.js
@@ -0,0 +1,29 @@
+import { error } from 'console';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+
+ const Connection = () => {
+
+    const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@mern-todo.dnlm0qk.mongodb.net/?retryWrites=true&w=majority`;
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true});
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected successfully');
+    })
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    }
+    )
+}
+
+export default Connection;
